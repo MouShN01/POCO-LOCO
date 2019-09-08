2fa6256c3be303f05d41e4ec41b6d666b78ebdd2
MouShN01 <sasha2701.cool@ukr.net>
2019-09-08 17:32:41 +0300

    Улучшеная программа

--- MyFavoritFilms.py
+++ MyFavoritFilms.py
@@ -1,10 +1,14 @@
-Films = dict([('Shawshank redemption', 'Drama'), ('Gost', 'Drama'), ('Pianist', 'Drama'), ('Gladiator', 'Advanture'), ('Inception', 'Advanture'), ('The Revenant', 'Advanture'), ('Lord of the rings', 'Fantasy'), ('Avatar','Fantasy'), ("Harry Potter", 'Fantasy')])
+Films = dict([('Shawshank redemption', 'Drama. Producer - Frank Darabont.'), ('Ghost', 'Drama. Producer - Lisa Weinstein.'), ('Pianist', 'Drama. Producer - Roman Polański.'), ('Gladiator', 'Advanture. Producer - Ridley Scott.'), ('Inception', 'Advanture. Producer - Christopher Jonathan.'), ('The Revenant', 'Advanture. Producer - Alejandro González.'), ('Lord of the rings', 'Fantasy. Producer - Peter Robert Jackson.'), ('Avatar','Fantasy. Producer - James Cameron.'), ("Harry Potter", 'Fantasy. Producer - Christopher Columbus.')])
+i = 1
+for film in Films:
+    print(str(i) + '. ' + film)
+    i +=1
 
-f = str(input('Введите название фильма: '))
+f = str(input('Введите название фильма для подробной информации: '))
 
 Film = [f]
 for film in Film:
     if film in Films:
-        print('Фильм в вашем списке ' + f + ' - ' + Films[film])
+        print('Фильм в вашем списке: ' + f + ' - ' + Films[film])
     else:
-        print('Фильма нету')
+        print('Такого фильма нету в списке.')
